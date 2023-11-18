86e5218deb2415b47de2388a2546dfb52e6640a4
Sergey Barkhatov <sergeybarkhat@yandex.ru>
2023-11-18 13:28:34 +0100

    Залить 15 проект

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -149,8 +149,6 @@ const login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      // res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true, sameSite: true });
-      // res.status(SUCCESS).send({ message: 'Авторизация успешна' });
       res.status(SUCCESS).send({ token });
     })
     .catch(next);
